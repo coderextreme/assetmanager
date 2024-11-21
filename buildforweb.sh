cf62f5bd5e482b9cb6ccd5341de44976a9438c5f
John W Carlson <yottzumm@gmail.com>
2024-11-21 04:05:21 -0600

    deleted

--- buildforweb.sh
+++
@@ -1,3 +0,0 @@
-nim c -d:emscripten -d:release -d:GraphicsApiOpenGlEs3 -d:NaylibWebAsyncify -d:NaylibWebResources flowers_oop2.nim
-echo http://localhost:1337/flowers_oop2.nim.html
-nimhttpd -H:"Cross-Origin-Opener-Policy: same-origin" -H:"Cross-Origin-Embedder-Policy: require-corp"
